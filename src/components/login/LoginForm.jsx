7ddf71cc69ae497025b3e7aac27b9ba1fb24c692
Artemichi <Artemchugainov@gmail.com>
2021-02-11 17:10:30 +0500

    login screen layout + loginform

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,15 +1,73 @@
 import React from "react";
+// ant
 import Card from "antd/lib/card";
+import Form from "antd/lib/form/Form";
+import FormItem from "antd/lib/form/FormItem";
+import Input from "antd/lib/input/Input";
+import InputPassword from "antd/lib/input/Password";
+import Button from "antd/lib/button/button";
+import UserOutlined from "@ant-design/icons/UserOutlined";
+import LockOutlined from "@ant-design/icons/LockOutlined";
+// img
+import logo from "../../assets/logo.svg";
 
 const LoginForm = () => {
+  const [loading, setLoading] = React.useState(false);
+
+  const onFinish = () => {
+    setLoading(prev => !prev);
+  };
+
   return (
-    <>
-      <Card
-        title="Единая цифровая платформа"
-        bordered={false}
-        headStyle={{ textAlign: "center" }}
-      ></Card>
-    </>
+    <Card
+      title={
+        <>
+          <img src={logo} alt="rigintel_logo" />
+          <br />
+          Единая цифровая платформа
+        </>
+      }
+      bordered={false}
+      headStyle={{ textAlign: "center" }}
+      hoverable
+    >
+      <Form
+        name="normal_login"
+        className="login-form"
+        size="large"
+        wrapperCol={{
+          span: 24,
+          offset: 0,
+          lg: { span: 20, offset: 2 },
+        }}
+        onFinish={onFinish}
+      >
+        <FormItem
+          name="username"
+          rules={[{ required: true, message: "Пожалуйста введите имя пользователя" }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Имя пользователя"
+          />
+        </FormItem>
+        <FormItem
+          name="password"
+          rules={[{ required: true, message: "Пожалуйста введите пароль" }]}
+        >
+          <InputPassword
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Пароль"
+          />
+        </FormItem>
+        <FormItem>
+          <Button type="primary" htmlType="submit" className="login-form-button" loading={loading}>
+            Войти
+          </Button>
+        </FormItem>
+      </Form>
+    </Card>
   );
 };
 
--- src/components/login/LoginLayout.jsx
+++ src/components/login/LoginLayout.jsx
@@ -2,9 +2,11 @@ import React from "react";
 // ant
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
-// comp
+// components
 import LoginForm from "./LoginForm";
-import isometric from "../../assets/isometric";
+// img
+import bg from "../../assets/bg.svg";
+import logo from "../../assets/logo.svg";
 
 const LoginLayout = () => {
   const mixin = {
@@ -15,12 +17,23 @@ const LoginLayout = () => {
       span: 0,
       md: 4,
     },
+    bg: {
+      backgroundImage: `url(${bg})`,
+      backgroundSize: "cover",
+      backgroundPosition: "100% 100%",
+    },
+    logo: {
+      marginLeft: "4em",
+      marginTop: "2em",
+    },
   };
 
   return (
-    <Row style={{ ...mixin.h_screen }}>
-      <Col span={0} md={12} style={{ background: url(isometric) }}></Col>
-      <Col span={24} md={12} style={{ background: "green" }}>
+    <Row style={{ ...mixin.h_screen, ...mixin.bg }}>
+      <Col span={0} lg={12} style={{}}>
+        <img src={logo} style={{ ...mixin.logo }} alt="rigintel_logo" />
+      </Col>
+      <Col span={24} lg={12} style={{}}>
         <Row align="middle" justify="center" style={{ ...mixin.h_screen }}>
           <Col {...mixin.form_gap} />
           <Col span={24} md={16}>
